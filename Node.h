a7bc22e6b54573d52ea280e1ab9f15175e38cd47
ejscribner <ejscribner@gmail.com>
2018-09-24 14:36:52 -0400

    comment blocks added

---
+++ Node.h
@@ -0,0 +1,7 @@
+/*
+CSE 109: Fall 2018
+Elliot Scribner
+ejs320
+Program creates nodes of a tree and traverses said tree
+Program #3
+*/
--- prog3.c
+++ prog3.c
@@ -1,3 +1,11 @@
+/*
+CSE 109: Fall 2018
+Elliot Scribner
+ejs320
+Program creates nodes of a tree and traverses said tree
+Program #3
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
